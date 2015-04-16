f8c99775b733a85b1835ff2d5a6ebd18ce7d854e
MisterrMe <a-r-m-a-d-a@mail.ru>
2015-04-16 22:57:41 +0300

    Create LabRab6_Launcher.cpp

---
+++ LabRab6_Launcher.cpp
@@ -0,0 +1,27 @@
+#include <conio.h>
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+int main()
+{
+	HANDLE hEvent;
+	setlocale(LC_ALL, "rus");
+	// Открываем объект- событие,созданное в другом приложении
+	hEvent = OpenEvent(EVENT_ALL_ACCESS,TRUE,L"MyEvent1");
+	if (hEvent == 0)
+	{
+		cout << "Ошибка открытия объекта- события, созданного в другом приложе-нии";
+		_getch();
+		return 0;
+	}
+	cout << "Для запуска потоков в другом приложении нажмите любую клавишу" << endl;
+	_getch();
+	SetEvent(hEvent); // Объект-событие свободен
+	cout << "Для завершения потоков в другом приложении нажмите любую клавишу" << endl;
+	_getch();
+	ResetEvent(hEvent); // Объект-событие занят
+	cout << "Для выхода из приложения нажмите любую клавишу" << endl;
+	_getch();
+	return 0;
+}
